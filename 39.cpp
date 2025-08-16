9c0004ecef7c1a507f2bfffcda83947873554955
Jiyabisht <bishtjiya.2005@gmail.com>
2025-08-16 12:49:17 +0530

    Update 39.cpp

--- 39.cpp
+++ 39.cpp
@@ -1,63 +1,54 @@
 #include <iostream>
 using namespace std;
 
-const int MAX_SIZE = 100; // Maximum stack size
+// Maximum stack size
+const int MAX_SIZE = 100;
 
-class Stack {
-private:
-    int arr[MAX_SIZE];
-    int top;
-
-public:
-    Stack() {
-        top = -1; // Stack is empty at start
+// Push function
+void push(int arr[], int &top) {
+    if (top == MAX_SIZE - 1) {
+        cout << "Stack Overflow! Cannot push element." << endl;
+        return;
     }
 
-    // Push function
-    void push() {
-        if (top == MAX_SIZE - 1) {
-            cout << "Stack Overflow! Cannot push element." << endl;
-            return;
-        }
+    int value;
+    cout << "Enter value to push: ";
+    cin >> value;
 
-        int value;
-        cout << "Enter value to push: ";
-        cin >> value;
+    top = top + 1;
+    arr[top] = value;
+    cout << value << " pushed to stack." << endl;
+}
 
-        top = top + 1;         // first increment top
-        arr[top] = value;      // then insert element
-        cout << value << " pushed to stack." << endl;
+// Pop function
+void pop(int arr[], int &top) {
+    if (top == -1) {
+        cout << "Stack Underflow! Cannot pop element." << endl;
+        return;
     }
 
-    // Pop function
-    void pop() {
-        if (top == -1) {
-            cout << "Stack Underflow! Cannot pop element." << endl;
-            return;
-        }
+    int value = arr[top];
+    top = top - 1;
+    cout << value << " popped from stack." << endl;
+}
 
-        int value = arr[top];  // get the top element
-        top = top - 1;         // then decrement top
-        cout << value << " popped from stack." << endl;
+// Display function
+void display(const int arr[], int top) {
+    if (top == -1) {
+        cout << "Stack is empty." << endl;
+        return;
     }
 
-    // Display function
-    void display() {
-        if (top == -1) {
-            cout << "Stack is empty." << endl;
-            return;
-        }
-
-        cout << "Stack elements: ";
-        for (int i = top; i >= 0; i--) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
+    cout << "Stack elements: ";
+    for (int i = top; i >= 0; i--) {
+        cout << arr[i] << " ";
     }
-};
+    cout << endl;
+}
 
 int main() {
-    Stack s;
+    int stack_array[MAX_SIZE];
+    int top = -1;
     int choice;
 
     while (true) {
@@ -65,11 +56,20 @@ int main() {
         cin >> choice;
 
         switch (choice) {
-            case 1: s.push(); break;
-            case 2: s.pop(); break;
-            case 3: s.display(); break;
-            case 4: cout << "Exiting..." << endl; return 0;
-            default: cout << "Invalid choice!" << endl;
+            case 1:
+                push(stack_array, top);
+                break;
+            case 2:
+                pop(stack_array, top);
+                break;
+            case 3:
+                display(stack_array, top);
+                break;
+            case 4:
+                cout << "Exiting..." << endl;
+                return 0;
+            default:
+                cout << "Invalid choice!" << endl;
         }
     }
 }
