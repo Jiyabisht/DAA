a59c683626e4c3fe5c00bd41c000fecb8643c442
Jiyabisht <bishtjiya.2005@gmail.com>
2025-10-06 02:18:07 +0530

    Update 46.cpp

--- 46.cpp
+++ 46.cpp
@@ -35,3 +35,51 @@ int main() {
 
     return 0;
 }
+
+
+
+
+//using stack as a counter to check the parenthesis
+
+#include <iostream>
+#include <string>
+#include <stack> // Needed for the stack data structure
+
+using namespace std;
+
+string removeOuterParentheses(string s) {
+    string result = "";
+    stack<char> s_stack; 
+
+    for (int i = 0; i < s.length(); i++) {
+        char ch = s[i]; 
+
+        if (ch == '(') {
+            if (!s_stack.empty()) {
+                result += ch; 
+            }
+            s_stack.push(ch);
+        } else if (ch == ')') {
+            s_stack.pop();
+            
+            if (!s_stack.empty()) {
+                result += ch; 
+            }
+        }
+    }
+    
+    return result;
+}
+
+int main() {
+    string user_input;
+    cout << "Enter a Valid Parentheses String (e.g., (()())(())): ";
+    cin >> user_input; 
+    
+    string output = removeOuterParentheses(user_input);
+    
+    cout << "\nOriginal String: " << user_input << endl;
+    cout << "Result String:   " << output << endl;
+
+    return 0;
+}
